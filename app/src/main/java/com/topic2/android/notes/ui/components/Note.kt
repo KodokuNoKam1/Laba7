6caa344d4ea389882b3c26e2b9e451b7db49ca15
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2024-03-31 13:42:16 +0300

    Note_change

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -1,5 +1,5 @@
 package com.topic2.android.notes.ui.components
-
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
@@ -8,14 +8,14 @@ import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.topic2.android.notes.theme.rwGreen
-import androidx.compose.foundation.background
 
-@Composable fun Note(){
+@Composable fun Note() {
     Row(modifier = Modifier.fillMaxWidth()) {
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(rwGreen)
+        NoteColor(
+            color = rwGreen,
+            size =40.dp,
+            padding =4.dp,
+            border=1.dp
         )
         Column(modifier = Modifier.weight(1f)) {
             Text(text = "Заголовок", maxLines = 1)
