b158e3abf12fb8892a864f01dda67874342a0250
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2024-05-01 15:21:04 +0300

    Edit_NoteScreen

--- app/src/main/java/com/topic2/android/notes/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/screens/NotesScreen.kt
@@ -1,5 +1,4 @@
 package com.topic2.android.notes.ui.screens
-
 import androidx.compose.runtime.Composable
 import com.topic2.android.notes.viewmodel.MainViewModel
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,23 +13,43 @@ import com.topic2.android.notes.domain.model.NoteModel
 import com.topic2.android.notes.ui.components.Note
 import android.annotation.SuppressLint
 import androidx.compose.material.*
+import androidx.compose.runtime.rememberCoroutineScope
+import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.ui.components.AppDrawer
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun NotesScreen(
     viewModel: MainViewModel
-    ) {
-        val notes: List<NoteModel> by viewModel
-            .notesNotInTrash
-            .observeAsState(listOf())
+) {
+    val notes: List<NoteModel> by viewModel
+        .notesNotInTrash
+        .observeAsState(listOf())
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
 
     Scaffold (topBar = {
         TopAppBar(
             title = "Notes",
-                    icon = Icons.Filled.List,
-                    onIconClick = {}
-                )
+            icon = Icons.Filled.List,
+            onIconClick = {
+                coroutineScope.launch {
+                    scaffoldState.drawerState.open()
+                }
+            }
+        )
     },
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            AppDrawer(
+                currentScreen = Screen.Notes, closeDrawerAction = {
+                    coroutineScope.launch {
+                        scaffoldState.drawerState.close()
+                    }
+                }
+            )
+        },
         content = {
             if (notes.isNotEmpty()) {
                 NotesList(
@@ -42,35 +61,33 @@ fun NotesScreen(
             }
         }
     )
-        }
-
-        @Composable
-        private fun NotesList(
-            notes: List<NoteModel>,
-            onNoteCheckedChange: (NoteModel) -> Unit,
-            onNoteClick:(NoteModel) -> Unit
-        ){
-            LazyColumn{
-                items(count = notes.size){noteIndex ->
-                    val note = notes[noteIndex]
-                    Note(note = note,
-                        onNoteClick = onNoteClick,
-                        onNoteCheckedChange = onNoteCheckedChange
-                    )
-                }
-            }
-        }
-
-        @Preview
-        @Composable
-        private fun  NotesListPreview(){
-            NotesList(
-                notes = listOf(
-                    NoteModel(1,"Note 1", "Content 1", null),
-                    NoteModel(2,"Note 2", "Content 2", false) ,
-                    NoteModel(3,"Note 1", "Content 3", true)
-                ),
-                onNoteCheckedChange = {},
-                onNoteClick = {}
+}
+@Composable
+private fun NotesList(
+    notes: List<NoteModel>,
+    onNoteCheckedChange: (NoteModel) -> Unit,
+    onNoteClick:(NoteModel) -> Unit
+){
+    LazyColumn{
+        items(count = notes.size){noteIndex ->
+            val note = notes[noteIndex]
+            Note(note = note,
+                onNoteClick = onNoteClick,
+                onNoteCheckedChange = onNoteCheckedChange
             )
         }
+    }
+}
+@Preview
+@Composable
+private fun  NotesListPreview(){
+    NotesList(
+        notes = listOf(
+            NoteModel(1,"Note 1", "Content 1", null),
+            NoteModel(2,"Note 2", "Content 2", false) ,
+            NoteModel(3,"Note 1", "Content 3", true)
+        ),
+        onNoteCheckedChange = {},
+        onNoteClick = {}
+    )
+}
